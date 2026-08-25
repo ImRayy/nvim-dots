8af1f0daf582fec9e06eb517c23c7fdc247cfa02
ImRayy <souradeep_roy@pm.me>
2026-08-25 13:19:39 +0530

    move taplo formatter to its own entry
    Split taplo config out of the biome formatter block so it applies independently.

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -35,26 +35,27 @@ return {
           "--no-errors-on-unmatched",
           "$FILENAME",
         },
-        taplo = {
-          command = "taplo",
-        -- stylua: ignore
-        args = {
-          "fmt",
-          "--option", "align_entries=true",
-          "--option", "align_comments=true",
-          "--option", "reorder_keys=true",
-          "--option", 'indent_string="  "',
-          "--option", "column_width=100",
-          "--option", "trailing_newline=true",
-          "--option", "array_auto_expand=true",
-          "--option", "array_auto_collapse=true",
-          "-"
-        },
-        },
         stdin = false,
         condition = function(ctx)
           return find_root_file({ "biome.json" }, ctx.filename)
         end,
+      },
+
+      taplo = {
+        command = "taplo",
+          -- stylua: ignore
+          args = {
+            "fmt",
+            "--option", "align_entries=true",
+            "--option", "align_comments=true",
+            "--option", "reorder_keys=true",
+            "--option", 'indent_string="  "',
+            "--option", "column_width=100",
+            "--option", "trailing_newline=true",
+            "--option", "array_auto_expand=true",
+            "--option", "array_auto_collapse=true",
+            "-"
+          },
       },
 
       prettierd = {
